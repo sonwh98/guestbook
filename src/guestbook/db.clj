3da7b82460d93e0d67c3866fa9b3191e1785230c
sto <son.c.to@gmail.com>
2014-09-13 23:43:18 -0400

    rm extra () pair

--- src/guestbook/db.clj
+++ src/guestbook/db.clj
@@ -1,7 +1,7 @@
 (ns guestbook.db)
 
 (require '[datomic.api :as datomic])
-(def uri "datomic:mem://databank")
+(def uri "datomic:mem://guestbook")
 
 (defn db-exists? [uri] (not (datomic/create-database uri)))
 (defn get-connection [] (datomic/connect uri))
@@ -9,7 +9,7 @@
 (defn do-transact [data-struct] @(datomic/transact (get-connection) data-struct))
 
 (defn import-test-data []
-  (if-not (db-exists?)
-    (let [schema (read-string (slurp ("resources/guestbook-schema.edn")))]
+  (if-not (db-exists? uri)
+    (let [schema (read-string (slurp "resources/guestbook-schema.edn"))]
       (println schema)
       (do-transact schema))))
